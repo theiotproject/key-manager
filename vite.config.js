1255cea351f21138e075e079cc50878ecaf08e19
DamianProchaska <damian.prochaska@gmail.com>
2022-07-05 15:22:50 +0200

    add authentication

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+        }),
         react(),
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
     ],
 });
