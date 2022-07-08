967e561c58837c36124e747a403d3e3ede267371
suawek013 <sojkaslawek123@gmail.com>
2022-07-08 15:59:12 +0200

    add events model

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\EventController;
+use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+Route::post('/events', [EventController::class, 'store']);
+
+Route::get('/events/{id}', function($id) {
+    $event = Event::find($id);
+    return $event;
+});
+
+Route::post('/events', [EventController::class, 'store']);
+
+Route::get('/events/{id}', function($id) {
+    $event = Event::find($id);
+    return $event;
 });
