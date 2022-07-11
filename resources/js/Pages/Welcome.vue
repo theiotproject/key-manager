471c430b83af085419a301096a790d9fb9318bc9
suawek013 <sojkaslawek123@gmail.com>
2022-07-11 17:09:22 +0200

    change logo & update ui

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -12,23 +12,43 @@ defineProps({
 <template>
     <Head title="Welcome" />
     <nav
-        class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800"
+        class="m-5 bg-white border-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800"
     >
         <div
             class="container flex flex-wrap justify-between items-center mx-auto"
         >
             <img
-                src="../../../public/images/qwarelogo.png"
-                class="mr-3 h-6 sm:h-9"
+                src="../../../public/images/qwarelogotransparent.png"
+                class="ml-32 mr-3 h-6 sm:h-9"
                 alt="Qware logo"
             />
             <div class="flex md:order-2">
-                <button
-                    type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                    Login
-                </button>
+                <div v-if="canLogin" class="hidden px-6 py-4 sm:block">
+                    <Link
+                        v-if="$page.props.user"
+                        :href="route('dashboard')"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Dashboard
+                    </Link>
+
+                    <template v-else>
+                        <Link
+                            :href="route('login')"
+                            class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                        >
+                            Login
+                        </Link>
+
+                        <Link
+                            v-if="canRegister"
+                            :href="route('register')"
+                            class="ml-3 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                        >
+                            Register
+                        </Link>
+                    </template>
+                </div>
                 <button
                     data-collapse-toggle="mobile-menu-4"
                     type="button"
@@ -84,7 +104,7 @@ defineProps({
                         <a
                             href="#"
                             class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >About</a
+                            >About Us</a
                         >
                     </li>
                     <li>
@@ -98,7 +118,7 @@ defineProps({
                         <a
                             href="#"
                             class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >Contact</a
+                            >Contact Us</a
                         >
                     </li>
                 </ul>
@@ -106,71 +126,28 @@ defineProps({
         </div>
     </nav>
 
-    <div class="">
-        <!--
-        <div class="heading">
-            <div class="title-img">
-                <img src="" />
-            </div>
-            <div class="title-text">
-                <h1>Qware</h1>
-            </div>
-        </div>
-        <div
-            v-if="canLogin"
-            class="hidden fixed top-0 right-0 px-6 py-4 sm:block"
-        >
-            <Link
-                v-if="$page.props.user"
-                :href="route('dashboard')"
-                class="text-sm text-gray-700 underline"
-            >
-                Dashboard
-            </Link>
-
-            <template v-else>
-                <Link
-                    :href="route('login')"
-                    class="text-sm text-gray-700 underline"
-                >
-                    Log in
-                </Link>
-
-                <Link
-                    v-if="canRegister"
-                    :href="route('register')"
-                    class="ml-4 text-sm text-gray-700 underline"
-                >
-                    Register
-                </Link>
-            </template>
-        </div> -->
-
+    <div class="mt-24">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div
-                class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg"
-            >
+            <div class="mt-8 bg-white sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="p-6">
-                        <div class="">
+                        <div class="description">
                             <h2>Why use keys if you have a phone?</h2>
                             <p>
-                                Deidcated resources, full root access, & easy
-                                scaling. It’s the virtual private server you’ve
-                                been cravin
+                                Dedicated website & mobile application, works
+                                out of the box and is easy to manage. It’s the
+                                most convenient tool you’ve been craving
                             </p>
-                            <button
-                                class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                            <Link
                                 :href="route('login')"
+                                class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
                             >
                                 START
-                            </button>
+                            </Link>
                         </div>
                     </div>
 
-                    <div
-                        class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l"
-                    >
+                    <div class="p-6">
                         <img
                             src="../../../public/images/qrcodebusiness.jpg"
                             alt="QR Code business"
@@ -182,4 +159,30 @@ defineProps({
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap");
+
+.bg-white {
+    background-color: #f7f7f7;
+}
+.description h2 {
+    font-size: 48px;
+    font-weight: 700;
+    color: #03045e;
+    font-family: "Ubuntu", sans-serif;
+}
+.description p {
+    opacity: 70%;
+    color: #03045e;
+    margin-top: 1.5rem;
+    margin-bottom: 1.5rem;
+}
+</style>
+<script>
+export default {
+    name: "About",
+    created() {
+        document.body.style.backgroundColor = "#f7f7f7";
+    },
+};
+</script>
