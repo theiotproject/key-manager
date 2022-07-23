f78f5ebd52666fd9a97f765e72221a19a9eab06b
DamianProchaska <damian.prochaska@gmail.com>
2022-07-23 09:43:21 +0200

    delete example unit test function

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,8 +11,8 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_that_true_is_true()
-    {
-        $this->assertTrue(true);
-    }
+//    public function test_that_true_is_true()
+//    {
+//        $this->assertTrue(true);
+//    }
 }
