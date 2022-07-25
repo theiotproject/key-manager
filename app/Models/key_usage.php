4a9b5ca4eb84fe88135c8bb8421bd9437d3132c6
DamianProchaska <damian.prochaska@gmail.com>
2022-07-25 11:23:50 +0200

    create key_usages model and table

---
+++ app/Models/key_usage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class key_usage extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_07_25_092146_create_key_usages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('key_usages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('virtual_key_id');
+            $table->boolean('access_granted');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('key_usages');
+    }
+};
