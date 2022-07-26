1907435674ae26c841ef96393ec56c5664eff032
suawek013 <sojkaslawek123@gmail.com>
2022-07-26 10:45:56 +0200

    add back button

--- resources/js/Components/Home/HomeHeader.vue
+++ resources/js/Components/Home/HomeHeader.vue
@@ -7,7 +7,7 @@ const showingNavigationDropdown = ref(false);
 <template>
     <Head title="Welcome" />
     <nav
-        class="m-5 mt-0 bg-white border-200 px-2 sm:px-4 py-1.5 rounded dark:bg-gray-800"
+        class="m-5 mt-0 bg-white border-200 px-2 sm:px-4 py-1.5 xl:py-3 rounded dark:bg-gray-800"
     >
         <div
             class="container flex flex-wrap justify-between items-center mx-auto"
@@ -147,6 +147,7 @@ const showingNavigationDropdown = ref(false);
                     </li>
                     <li>
                         <a
+                            target="_blank"
                             href="http://theiotproject.com/"
                             class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                             >Blog</a
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -23,16 +23,28 @@ defineProps({
     </header>
     <span id="home"></span>
     <main class="relative">
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <section class="vh-100">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 xl:px-0">
+            <!-- <section class="h-screen">
                 <HomeMain />
             </section>
             <span id="about"></span>
-            <section class="vh-100 bg-white">
+            <section class="h-screen bg-white">
                 <About />
             </section>
             <span id="contact"></span>
-            <section class="vh-100" id="contact">
+            <section class="md:h-screen" id="contact">
+                <Contact />
+            </section> -->
+
+            <section class="">
+                <HomeMain />
+            </section>
+            <span id="about"></span>
+            <section class="bg-white">
+                <About />
+            </section>
+            <span id="contact"></span>
+            <section class="" id="contact">
                 <Contact />
             </section>
         </div>
