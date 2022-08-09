60b1abc8ff0203343c89fa86f9f02cdceecd6d9f
DamianProchaska <damian.prochaska@gmail.com>
2022-08-09 19:58:16 +0200

    Add intro.js on every dashboard refresh

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { onMounted } from "vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import { Link } from "@inertiajs/inertia-vue3";
 import GatesWidget from "./Widgets/GatesWidget.vue";
@@ -6,32 +7,56 @@ import VirtualKeysWidget from "./Widgets/VirtualKeysWidget.vue";
 import UsersWidget from "./Widgets/UsersWidget.vue";
 import EventsWidget from "./Widgets/EventsWidget.vue";
 import HomeHeader from "../Components/Home/HomeHeader.vue";
+import introJs from "intro.js";
+
+onMounted(() => {
+  let tour = introJs();
+  tour.setOptions({
+    steps: [
+      {
+        title: `Welcome to Key Manager!`,
+        intro: `Let me show you around! I'll be quick, I promise!`,
+      },
+      {
+        intro: `Here you can add your first gate by clicking this button`,
+        element: document.querySelector("#firstStepTour"),
+      },
+      {
+        intro: `Then you can give your teammates access to your gates by creating Virtual Keys`,
+        element: document.querySelector("#secondStepTour"),
+      },
+    ],
+  });
+  tour.start();
+});
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="parent gap-x-5 gap-y-5">
-                    <GatesWidget class="max-h-100 area1" v-bind:attrs="attrs" />
-                    <UsersWidget class="max-h-100 area2" v-bind:attrs="attrs" />
-                    <VirtualKeysWidget
-                        class="max-h-100 area3"
-                        v-bind:attrs="attrs"
-                    />
-                    <EventsWidget
-                        class="max-h-200 area4"
-                        v-bind:attrs="attrs"
-                    />
-                </div>
-            </div>
+  <AppLayout title="Dashboard">
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Dashboard
+      </h2>
+    </template>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="parent gap-x-5 gap-y-5">
+          <GatesWidget
+            class="max-h-100 area1"
+            v-bind:attrs="attrs"
+            id="firstStepTour"
+          />
+          <UsersWidget class="max-h-100 area2" v-bind:attrs="attrs" />
+          <VirtualKeysWidget
+            id="secondStepTour"
+            class="max-h-100 area3"
+            v-bind:attrs="attrs"
+          />
+          <EventsWidget class="max-h-200 area4" v-bind:attrs="attrs" />
         </div>
-    </AppLayout>
+      </div>
+    </div>
+  </AppLayout>
 </template>
 
 <script>
@@ -39,90 +64,90 @@ import GateShow from "./Gates/Show.vue";
 import CreateGateForm from "./Gates/Partials/CreateGateForm.vue";
 
 export default {
-    components: {
-        GateShow,
-        CreateGateForm,
+  components: {
+    GateShow,
+    CreateGateForm,
+  },
+  data() {
+    return {
+      component: "GateShow",
+      attrs: this.$attrs,
+    };
+  },
+  methods: {
+    toggle() {
+      if (this.component === "GateShow") {
+        this.component = "CreateGateForm";
+      } else {
+        this.component = "GateShow";
+      }
     },
-    data() {
-        return {
-            component: "GateShow",
-            attrs: this.$attrs,
-        };
-    },
-    methods: {
-        toggle() {
-            if (this.component === "GateShow") {
-                this.component = "CreateGateForm";
-            } else {
-                this.component = "GateShow";
-            }
-        },
-    },
+  },
 };
 </script>
 
 <style>
 @media only screen and (min-width: 100px) {
-    .parent {
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: repeat(4, 1fr);
-    }
+  .parent {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(4, 1fr);
+  }
 
-    .area1 {
-        grid-area: 1 / 1 / 2 / 2;
-    }
-    .area2 {
-        grid-area: 2 / 1 / 3 / 2;
-    }
-    .area3 {
-        grid-area: 3 / 1 / 4 / 2;
-    }
-    .area4 {
-        grid-area: 4 / 1 / 5 / 2;
-    }
+  .area1 {
+    grid-area: 1 / 1 / 2 / 2;
+  }
+  .area2 {
+    grid-area: 2 / 1 / 3 / 2;
+  }
+  .area3 {
+    grid-area: 3 / 1 / 4 / 2;
+  }
+  .area4 {
+    grid-area: 4 / 1 / 5 / 2;
+  }
 }
 
 @media only screen and (min-width: 650px) {
-    .parent {
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(2, 1fr);
-    }
+  .parent {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+  }
 
-    .area1 {
-        grid-area: 1 / 1 / 2 / 2;
-    }
-    .area2 {
-        grid-area: 2 / 1 / 3 / 2;
-    }
-    .area3 {
-        grid-area: 1 / 2 / 2 / 3;
-    }
-    .area4 {
-        grid-area: 2 / 2 / 3 / 3;
-    }
+  .area1 {
+    grid-area: 1 / 1 / 2 / 2;
+  }
+  .area2 {
+    grid-area: 2 / 1 / 3 / 2;
+  }
+  .area3 {
+    grid-area: 1 / 2 / 2 / 3;
+  }
+  .area4 {
+    grid-area: 2 / 2 / 3 / 3;
+  }
 }
 
 @media only screen and (min-width: 1250px) {
-    .parent {
-        display: grid;
-        grid-template-columns: 0.8fr 1fr 1.1fr;
-        grid-template-rows: repeat(2, 1fr);
-    }
+  .parent {
+    display: grid;
+    grid-template-columns: 0.8fr 1fr 1.1fr;
+    grid-template-rows: repeat(2, 1fr);
+  }
 
-    .area1 {
-        grid-area: 1 / 1 / 2 / 2;
-    }
-    .area2 {
-        grid-area: 1 / 2 / 2 / 3;
-    }
-    .area3 {
-        grid-area: 2 / 1 / 3 / 3;
-    }
-    .area4 {
-        grid-area: 1 / 3 / 3 / 4;
-    }
+  .area1 {
+    grid-area: 1 / 1 / 2 / 2;
+  }
+  .area2 {
+    grid-area: 1 / 2 / 2 / 3;
+  }
+  .area3 {
+    grid-area: 2 / 1 / 3 / 3;
+  }
+  .area4 {
+    grid-area: 1 / 3 / 3 / 4;
+  }
 }
 
 /*.area1  gates*/
