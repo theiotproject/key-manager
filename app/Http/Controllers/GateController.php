550ffc36a8f80208955f57df55a7855f30d1e4be
suawek013 <sojkaslawek123@gmail.com>
2022-08-18 10:58:20 +0200

    fix qr code

--- app/Http/Controllers/GateController.php
+++ app/Http/Controllers/GateController.php
@@ -308,7 +308,7 @@ class GateController extends Controller
             $teamCode = $team->team_code;
             $qrcode = "OPEN:ID:{$guid};VF:{$validFrom};VT:{$validTo};L:{$gateSerialNumber};;";
             $hashQrcode = hash('sha256', $qrcode . $teamCode);
-            $finalQrcode = $qrcode . "S:" . $hashQrcode;
+            $finalQrcode = $qrcode . "S:" . $hashQrcode . ";";
 
             MQTT::publish("iotlock/v1/{$teamCode}/control/{$gateSerialNumber}", $finalQrcode);
             return back(303);
